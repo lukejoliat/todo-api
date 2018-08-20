29f272c3b1c83345cb8d09d3dd9a77f35a17cf49
Luke Joliat <lukejoliat@dswinc.com>
2018-08-20 09:26:10 -0400

    Add express validator and babel

--- routes/routes-user.mjs
+++ routes/routes-user.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import { db } from '../config/config';
 import User from '../models/User';
 const router = express.Router();
-
+// no validation
 router.get('/', (req, res, next) => {
   const usersRef = db.collection('users');
   usersRef
@@ -19,14 +19,15 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
+// validate id is in payload
 router.get('/:id', (req, res, next) => {
-  const usersRef = db
-    .collection('users')
-    .doc(req.params.id || null)
+  const id = req.params.id;
+  db.collection('users')
+    .doc(req.params.id)
     .get()
     .then(doc => {
       if (!doc.exists) {
-        res.status(400).json({ message: 'No user found!' });
+        res.status(400).json({ message: `No user found with id: ${id}!` });
       } else {
         res.status(200).json(doc.data());
       }
@@ -34,18 +35,65 @@ router.get('/:id', (req, res, next) => {
     .catch(next);
 });
 
+// validate structure of data
 router.post('/create', (req, res, next) => {
   const data = new User(req.body);
   if (!data.invalid) {
-    const setDoc = db
-      .collection('users')
+    db.collection('users')
       .doc(req.body.first)
       .set(JSON.parse(JSON.stringify(data)))
       .then(response => res.status(200).json(response))
       .catch(next);
   } else {
-    res.status(500).json({ message: 'missing required fields' });
+    res.status(500).json({ message: 'invalid payload' });
   }
 });
+
+// validate id exists
+router.delete('/delete/:id', (req, res, next) => {
+  const id = req.params.id;
+  db.collection('users')
+    .doc(id)
+    .delete()
+    .then(response => res.status(200).json(response))
+    .catch(next);
+});
+
+// validate field(s) is/are in model and of right data type
+router.patch('/update/:id', (req, res, next) => {
+  const id = req.params.id,
+    { first, last } = req.body,
+    userRef = db.collection('users').doc(id);
+  userRef
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        const data = new User({
+          first: first || doc.data().first,
+          last: last || doc.data().last
+        });
+        if (!data.invalid) {
+          userRef
+            .update(JSON.parse(JSON.stringify(data)))
+            .then(response => res.status(200).json(response))
+            .catch(next);
+        } else {
+          res.status(500).json({ message: 'invalid payload' });
+        }
+      } else {
+        res.status(500).json({ message: 'document does not exist' });
+      }
+    })
+    .catch(next);
+});
+
+// failIfInvalid = (req, res, next) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(422).json({ errors: errors.array() });
+//   } else {
+//     next();
+//   }
+// };
 
 export default router;
